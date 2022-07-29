3270aae61cc319645c6fdc86f487c2513e8daef3
KamilMammadov <kamiltm@code.edu.az>
2022-07-29 19:59:32 +0400

    GetOwners and GetDrugists methods added to Repository

--- DrugStoreManagement/DrugStoreManagement/Database/Rpository/UserRepository.cs
+++ DrugStoreManagement/DrugStoreManagement/Database/Rpository/UserRepository.cs
@@ -24,19 +24,48 @@ namespace DrugStoreManagement.Database
 
         public static List<User> Users { get; set; } = new List<User>()
         {
-
-
         };
 
-      
+
 
         public static User Add(User user)
         {
             Users.Add(user);
             return user;
         }
-        
 
+        public static List<User> GetAll()
+        {
+            return Users;
+        }
+
+        public static List<Owner> GetOwners()
+        {
+            List<Owner> owners = new List<Owner>();
+            foreach (User user in Users)
+            {
+                if (user is Owner)
+                {
+                    owners.Add((Owner)user);
+                }
+            }
+
+            return owners;
+        }
+
+        public static List<Drugist> GetDrugists()
+        {
+            List<Drugist> drugists = new List<Drugist>();
+            foreach (User user in Users)
+            {
+                if (user is Drugist)
+                {
+                    drugists.Add((Drugist)user);
+                }
+            }
+
+            return drugists;
+        }
 
         public static void Delete(User user)
         {
@@ -45,13 +74,9 @@ namespace DrugStoreManagement.Database
 
 
 
-        public static List<User> GetAll()
-        {
-            return Users;
-        }
 
 
-       
+
 
     }
 }
